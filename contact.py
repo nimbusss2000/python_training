9c9c7a5b86c768ffff6abf741fd7367fe4b9c25e
nimbusss2000 <62185943+nimbusss2000@users.noreply.github.com>
2020-08-17 23:55:36 +0300

    a test has been created to add a contact

---
+++ contact.py
@@ -0,0 +1,9 @@
+
+class Contact:
+    def __init__(self, firstname, lastname, homephone, mobilephone, workphone, fax):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.fax = fax
---
+++ test_add_contact.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+from contact import Contact
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Chrome(executable_path=r'C:\gecko\chromedriver.exe')
+        self.wd.implicitly_wait(30)
+    
+    def test_add_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.create_contact(wd, Contact(firstname='firstname', lastname='lastname', homephone='homephone',
+                                        mobilephone='mobilephone', workphone='workphone', fax='fax'))
+        self.return_to_home_page(wd)
+        self.logout(wd)
+
+    def create_contact(self, wd, contact):
+        # init contact creation
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.homephone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact.workphone)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact.fax)
+        # submit_contact_creation
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self, wd):
+        wd.find_element_by_link_text("home page").click()
+
+    def login(self, wd, username="admin", password="secret"):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
+
+    def tearDown(self):
+        self.wd.quit()
+
+if __name__ == "__main__":
+    unittest.main()

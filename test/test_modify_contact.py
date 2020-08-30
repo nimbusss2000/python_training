a2096fc359033fc828807b373f70ce9aa76e0725
nimbusss2000 <62185943+nimbusss2000@users.noreply.github.com>
2020-08-30 18:54:18 +0300

    added new check for test_modify_contact.py

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,6 +2,9 @@
 from model.contact import Contact
 
 def test_modify_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create_contact(Contact(firstname='test for mod'))
     app.contact.modify_contact(Contact(firstname='new_firstname', lastname='new_lastname',
-                                       homephone='new_homephone',mobilephone='new_mobilephone',
+                                       homephone='new_homephone', mobilephone='new_mobilephone',
                                        workphone='new_workphone', fax='new_fax'))
+

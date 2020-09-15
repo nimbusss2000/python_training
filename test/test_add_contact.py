6230d91b6f16ae716a90ed584c25afc429cc4c36
nimbusss2000 <62185943+nimbusss2000@users.noreply.github.com>
2020-09-15 18:40:21 +0300

    tests for adding groups and contacts on boot add random lines

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from model.contact import Contact
+import pytest
+import random
+import string
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + ' '*10
+    return prefix + ''.join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_contact(app):
+testdata = [Contact(firstname=random_string('firstname_', 10), lastname=random_string('lastname_', 10),
+                    homephone=random_string('homephone', 7),  mobilephone=random_string('mobilephone', 7),
+                    workphone=random_string('workphone', 7), fax=random_string('fax', 5)) for i in range(3)]
+
+@pytest.mark.parametrize('cntct', testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, cntct):
     old_contacts = app.contact.get_contacts_list()
-    cntct = Contact(firstname='firstname', lastname='lastname', homephone='homephone',
-                                    mobilephone='mobilephone', workphone='workphone', fax='fax')
     app.contact.create_contact(cntct)
     assert len(old_contacts) + 1 == app.contact.count()
 

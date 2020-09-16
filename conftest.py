d3ec32483b28e7f8d3ed01889eb240b7632076d0
nimbusss2000 <62185943+nimbusss2000@users.noreply.github.com>
2020-09-16 13:47:40 +0300

    added command line options to specify browser type, application address and host

--- conftest.py
+++ conftest.py
@@ -9,11 +9,15 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption('--browser')
+    base_url = request.config.getoption('--baseUrl')
+    host_ip = request.config.getoption('--hostip')
+
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url, host_ip=host_ip)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url, host_ip=host_ip)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
@@ -27,3 +31,10 @@ def stop(request):
 
 # разделили фиксуру на две части: инициализацию и финализацию
 # (финализ. будет выполняться один раз в самом конце)
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='chrome')
+    parser.addoption('--baseUrl', action='store', default='http://localhost/addressbook/group.php')
+    parser.addoption('--hostip', action='store', default='127.0.0.1')
+
+
--- fixture/application.py
+++ fixture/application.py
@@ -5,8 +5,17 @@ from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Chrome(executable_path=r'C:\gecko\chromedriver1.exe')
+    def __init__(self, browser, base_url, host_ip):
+        if browser == 'chrome':
+            self.wd = webdriver.Chrome(executable_path=r'C:\gecko\chromedriver1.exe')
+        elif browser == 'firefox':
+            self.wd = webdriver.Firefox()
+        elif browser == 'ie':
+            self.wd = webdriver.Ie(executable_path=r'C:\gecko\IEDriverServer.exe')
+        else:
+            raise ValueError('unrecognized browser')
+        self.base_url = base_url
+        self.host_ip = host_ip
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
@@ -20,7 +29,7 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()

4ddbe38b484311392298d8ad463aa878082eb414
nimbusss2000 <62185943+nimbusss2000@users.noreply.github.com>
2020-09-22 20:47:30 +0300

    Implement loading test data from files-2

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,18 +3,7 @@
 from model.group import Group
 
 
-def test_add_group(app, data_groups):            # data_groups - загружаем из пакета data, модуля - groups
-    grp = data_groups
-    old_groups = app.group.get_group_list()
-    app.group.create(grp)
-    assert len(old_groups) + 1 == app.group.count()
-
-    new_groups = app.group.get_group_list()
-    old_groups.append(grp)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_add_group_json(app, json_groups):
+def test_add_group_json(app, json_groups):        # json_groups - загружаем из пакета data, модуля - groups.json
     grp = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(grp)
@@ -23,3 +12,16 @@ def test_add_group_json(app, json_groups):
     new_groups = app.group.get_group_list()
     old_groups.append(grp)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+
+
+# def test_add_group(app, data_groups):            # data_groups - загружаем из пакета data, модуля - groups
+#     grp = data_groups
+#     old_groups = app.group.get_group_list()
+#     app.group.create(grp)
+#     assert len(old_groups) + 1 == app.group.count()
+#
+#     new_groups = app.group.get_group_list()
+#     old_groups.append(grp)
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+

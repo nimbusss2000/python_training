9ae43843d0009599ac7014996fb865ac02522ee3
nimbusss2000 <62185943+nimbusss2000@users.noreply.github.com>
2020-10-07 23:18:51 +0300

    added tests for adding contact to group and remove contact from group-2

--- test/test_add_and_remove_contact_to_group.py
+++ test/test_add_and_remove_contact_to_group.py
@@ -1,14 +1,25 @@
 
 from model.contact import Contact
+from model.group import Group
 import random
 
 def test_add_c_to_g(app, db, orm):
-    contacts_list = db.get_contact_list()
-    some_contact = random.choice(contacts_list)
-    groups_list = db.get_group_list()
-    some_group = random.choice(groups_list)
+    groups = db.get_group_list()
+    some_group = random.choice(groups)
+    if len(groups) == 0:
+        app.group.create(Group(name='group_test'))
+    contacts = db.get_contacts_list()
+    if len(contacts) == 0:
+        app.contact.create_contact(Contact(firstname='contact_test'))
+    some_contacts = orm.get_contacts_not_in_group(some_group)
+
+    if len(some_contacts) == 0:
+        app.contact.create_contact(Contact(firstname='contact_test'))
+
+    some_contact = random.choice(some_contacts)
     old_list_c_in_g = orm.get_contacts_in_group(some_group)
     app.contact.add_contact_to_group(some_contact.id, some_group.id)
+
     new_list_c_in_g = orm.get_contacts_in_group(some_group)
     assert len(old_list_c_in_g) + 1 == len(new_list_c_in_g)
     old_list_c_in_g.append(some_contact)
@@ -18,18 +29,22 @@ def test_add_c_to_g(app, db, orm):
 def test_remove_c_to_g(app, db, orm):
     groups = db.get_group_list()
     some_group = random.choice(groups)
-    contacts = orm.get_contacts_in_group(some_group)
-    if contacts is None:
-        contacts_list = db.get_contact_list()
-        some_contact = random.choice(contacts_list)
-        app.contact.add_contact_to_group(some_contact.id, some_group.id)
+    if len(groups) == 0:
+        app.group.create(Group(name='group_test'))
+    contacts = db.get_contacts_list()
+    if len(contacts) == 0:
+        app.contact.create_contact(Contact(firstname='contact_test'))
     some_contact = random.choice(contacts)
+    contacts_in_group = orm.get_contacts_in_group(some_group)
+
+    if len(contacts_in_group) == 0:
+        app.contact.add_contact_to_group(some_contact.id, some_group.id)
+        contacts_in_group = orm.get_contacts_in_group(some_group)
+
+    contact_for_remove = random.choice(contacts_in_group)
     old_list_c_in_g = orm.get_contacts_not_in_group(some_group)
-    app.contact.remove_contact_from_group(some_contact.id, some_group.id)
+    app.contact.remove_contact_from_group(contact_for_remove.id, some_group.id)
     new_list_c_in_g = orm.get_contacts_not_in_group(some_group)
     assert len(old_list_c_in_g) + 1 == len(new_list_c_in_g)
-    new_list_c_in_g.remove(some_contact)
+    new_list_c_in_g.remove(contact_for_remove)
     assert sorted(old_list_c_in_g, key=Contact.id_or_max) == sorted(new_list_c_in_g, key=Contact.id_or_max)
-
-
-
